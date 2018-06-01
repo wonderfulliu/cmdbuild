76def48be8d7c8f9b02d085dbac22868c496a2ef
xingchendahai <xcdhly@163.com>
2018-06-01 15:15:28 +0800

    bug修改

--- src/pages/configMsg/configEdit.vue
+++ src/pages/configMsg/configEdit.vue
@@ -21,7 +21,7 @@
           </Row>
         </Header>
         <Content class="contentTable">
-          <div class="contentBody">
+          <div class="contentBody"> 
             <Table stripe
                    border
                    size="small"
@@ -40,7 +40,15 @@
                   共 {{ totalBar }} 条
                   </Col>
                   <Col span="2">
-                  <Button type="text" icon="chevron-left" @click="pageFirst" :disabled="firstCl" title="首页"></Button>
+                  <!-- <Button type="text" icon="chevron-left" @click="pageFirst" :disabled="firstCl" title="首页"></Button> -->
+                  <button title="首页"
+                          type="button"
+                          class="pageBtn"
+                          :class="{'disableBtn':firstCl}"
+                          :disabled="firstCl"
+                          @click="pageFirst">
+                    <i class="zIcon firstPage"></i>
+                  </button>
                   </Col>
                   <Col span="14" style="width: 190px;text-align: center">
                   <Page simple
@@ -51,7 +59,15 @@
                         @on-change="pageChange"></Page>
                   </Col>
                   <Col span="2">
-                  <Button type="text" icon="chevron-right" @click="pageLast" :disabled="lastCl" title="尾页"></Button>
+                  <!-- <Button type="text" icon="chevron-right" @click="pageLast" :disabled="lastCl" title="尾页"></Button> -->
+                  <button title="尾页"
+                        type="button"
+                        class="pageBtn"
+                        :class="{'disableBtn':lastCl}"
+                        :disabled="lastCl"
+                        @click="pageLast">
+                    <i class="zIcon lastPage"></i>
+                  </button>
                   </Col>
                 </Row>
                 </Col>
